46792194fdb4c4623a7185d4b1f735b4e9e17c0b
Rajashekar Ganiger <80900181+rajashekar-2002@users.noreply.github.com>
2024-04-26 15:05:17 +0000

    Add files via upload

---
+++ apigatway/apigatway/src/main/java/com/APIGateway/apigatway/ApigatwayApplication.java
@@ -0,0 +1,14 @@
+package com.APIGateway.apigatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApigatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatwayApplication.class, args);
+	}
+
+}
---
+++ apigatway/apigatway/src/test/java/com/APIGateway/apigatway/ApigatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIGateway.apigatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
